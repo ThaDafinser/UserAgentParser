ffdd0409f8057fea82ca354ba6427cd3afbf6cb2
Keckeis Martin <martin.keckeis1@gmail.com>
2016-03-15 10:44:25 +0100

    donatj integration tests

--- tests/integration/Provider/DonatjUAParserTest.php
+++ tests/integration/Provider/DonatjUAParserTest.php
@@ -8,15 +8,29 @@ use UserAgentParser\Provider\DonatjUAParser;
  */
 class DonatjUAParserTest extends AbstractProviderTestCase
 {
-    public function testFunctionReturnsArray()
+    /**
+     * @expectedException \UserAgentParser\Exception\NoResultFoundException
+     */
+    public function testNoResultFound()
     {
         $provider = new DonatjUAParser();
 
-        $result = \parse_user_agent('A real user agent...');
+        $result = $provider->parse('...');
+    }
 
-        $this->assertInternalType('array', $result);
-        $this->assertArrayHasKey('platform', $result);
-        $this->assertArrayHasKey('browser', $result);
-        $this->assertArrayHasKey('version', $result);
+    public function testRealResult()
+    {
+        $provider = new DonatjUAParser();
+
+        $result = $provider->parse('Mozilla/5.0 (Nintendo 3DS; U; ; en) Version/1.7552.EU');
+
+        $this->assertInstanceOf('UserAgentParser\Model\UserAgent', $result);
+
+        $rawResult = $result->getProviderResultRaw();
+
+        $this->assertInternalType('array', $rawResult);
+        $this->assertArrayHasKey('platform', $rawResult);
+        $this->assertArrayHasKey('browser', $rawResult);
+        $this->assertArrayHasKey('version', $rawResult);
     }
 }

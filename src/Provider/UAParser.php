91a95d8d39a8f048ee62537f005328e20d8bd48b
Martin Keckeis <martin.keckeis1@gmail.com>
2016-05-18 13:08:01 +0200

    getRealResult (#83)

--- src/Provider/UAParser.php
+++ src/Provider/UAParser.php
@@ -187,10 +187,7 @@ class UAParser extends AbstractProvider
     private function hydrateBot(Model\Bot $bot, \UAParser\Result\Client $resultRaw)
     {
         $bot->setIsBot(true);
-
-        if ($this->isRealResult($resultRaw->ua->family, 'bot', 'name') === true) {
-            $bot->setName($resultRaw->ua->family);
-        }
+        $bot->setName($this->getRealResult($resultRaw->ua->family, 'bot', 'name'));
     }
 
     /**
@@ -200,21 +197,11 @@ class UAParser extends AbstractProvider
      */
     private function hydrateBrowser(Model\Browser $browser, \UAParser\Result\UserAgent $uaRaw)
     {
-        if ($this->isRealResult($uaRaw->family) === true) {
-            $browser->setName($uaRaw->family);
-        }
+        $browser->setName($this->getRealResult($uaRaw->family));
 
-        if ($this->isRealResult($uaRaw->major) === true) {
-            $browser->getVersion()->setMajor($uaRaw->major);
-        }
-
-        if ($this->isRealResult($uaRaw->minor) === true) {
-            $browser->getVersion()->setMinor($uaRaw->minor);
-        }
-
-        if ($this->isRealResult($uaRaw->patch) === true) {
-            $browser->getVersion()->setPatch($uaRaw->patch);
-        }
+        $browser->getVersion()->setMajor($this->getRealResult($uaRaw->major));
+        $browser->getVersion()->setMinor($this->getRealResult($uaRaw->minor));
+        $browser->getVersion()->setPatch($this->getRealResult($uaRaw->patch));
     }
 
     /**
@@ -224,21 +211,11 @@ class UAParser extends AbstractProvider
      */
     private function hydrateOperatingSystem(Model\OperatingSystem $os, \UAParser\Result\OperatingSystem $osRaw)
     {
-        if ($this->isRealResult($osRaw->family) === true) {
-            $os->setName($osRaw->family);
-        }
+        $os->setName($this->getRealResult($osRaw->family));
 
-        if ($this->isRealResult($osRaw->major) === true) {
-            $os->getVersion()->setMajor($osRaw->major);
-        }
-
-        if ($this->isRealResult($osRaw->minor) === true) {
-            $os->getVersion()->setMinor($osRaw->minor);
-        }
-
-        if ($this->isRealResult($osRaw->patch) === true) {
-            $os->getVersion()->setPatch($osRaw->patch);
-        }
+        $os->getVersion()->setMajor($this->getRealResult($osRaw->major));
+        $os->getVersion()->setMinor($this->getRealResult($osRaw->minor));
+        $os->getVersion()->setPatch($this->getRealResult($osRaw->patch));
     }
 
     /**
@@ -248,13 +225,8 @@ class UAParser extends AbstractProvider
      */
     private function hydrateDevice(Model\Device $device, \UAParser\Result\Device $deviceRaw)
     {
-        if ($this->isRealResult($deviceRaw->model, 'device', 'model') === true) {
-            $device->setModel($deviceRaw->model);
-        }
-
-        if ($this->isRealResult($deviceRaw->brand, 'device', 'brand') === true) {
-            $device->setBrand($deviceRaw->brand);
-        }
+        $device->setModel($this->getRealResult($deviceRaw->model, 'device', 'model'));
+        $device->setBrand($this->getRealResult($deviceRaw->brand, 'device', 'brand'));
     }
 
     public function parse($userAgent, array $headers = [])

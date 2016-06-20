c5eecc12fa0b36eb0e2c0c54e9e0e33ac0b285c0
Keckeis Martin <martin.keckeis1@gmail.com>
2016-06-20 12:54:50 +0200

    fixing browscap update split

--- tests/integration/Provider/BrowscapLiteTest.php
+++ tests/integration/Provider/BrowscapLiteTest.php
@@ -1,7 +1,6 @@
 <?php
 namespace UserAgentParserTest\Integration\Provider;
 
-use BrowscapPHP\Browscap;
 use UserAgentParser\Provider\BrowscapLite;
 
 /**
@@ -31,27 +30,6 @@ class BrowscapLiteTest extends AbstractBrowscapTestCase
     public function testColdCacheException()
     {
         $provider = new BrowscapLite($this->getParserWithColdCache('lite'));
-
-        $result = $provider->parse('...');
-    }
-
-    /**
-     * @expectedException \UserAgentParser\Exception\InvalidArgumentException
-     * @expectedExceptionMessage You need to warm-up the cache first to use this provider
-     */
-    public function testColdCacheExceptionStillAfterGetBrowser()
-    {
-        $parser = $this->getParserWithColdCache('lite');
-
-        $result = $parser->getBrowser('Mozilla/5.0 (SMART-TV; X11; Linux armv7l) AppleWebkit/537.42 (KHTML, like Gecko) Chromium/48.0.1349.2 Chrome/25.0.1349.2 Safari/537.42');
-
-        // verify that browscap returned something
-        $this->assertInstanceOf('stdClass', $result);
-        // the return value is the default browser, since there is no cache
-        // https://github.com/browscap/browscap-php/issues/150#issuecomment-197197655
-        $this->assertEquals('Default Browser', $result->browser);
-
-        $provider = new BrowscapLite($parser);
 
         $result = $provider->parse('...');
     }
